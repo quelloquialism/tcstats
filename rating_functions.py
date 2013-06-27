e9c371755a6f76aea2d9e8c3f62025a3b4ee6a01
Nathan Watson <nathan.watson@openx.com>
2013-06-26 21:43:17 -0700

    rating_functions.py: rating_color, first pass of get_accomplishments, refactor stupid_adj/get_compliment

--- rating_functions.py
+++ rating_functions.py
@@ -17,42 +17,79 @@ def ordinal(i):
     suffix = "th"
   return str(i) + suffix
 
-def stupid_adjective(rating):
-  compliments = {
-    35: "a splendiferatageneous",
-    34: "an absolutely elite",
-    33: "a world beating",
-    32: "a truly amazing",
-    31: "a really quite very smart",
-    30: "an unoverestimatable",
-    29: "a soon-to-be-targeted",
-    28: "an upper crust",
-    27: "a smashing",
-    26: "an impressive",
-    25: "a savvy",
-    24: "a weighty-brained",
-    23: "an algorithmically inclined",
-    22: "a top",
-    21: "a soon-to-be-red",
-    20: "a problem gobbling",
-    19: "a solid",
-    18: "a good smart",
-    17: "a tough",
-    16: "a two-ply extra strong",
-    15: "a strong",
-    14: "a soon-to-be-yellow",
-    13: "a smart",
-    12: "a well liked",
-    11: "a soon-to-be-blue",
-     5: "an up-and-coming",
-     0: "a scruffy fighter of a"
-  }
+def get_compliment(rating):
+  compliments = [
+    (40, "an unreal super-hyper-target"),
+    (37, "an indisputably world-class"),
+    (36, "an off-the-charts awesome"),
+    (35, "a splendiferatageneous"),
+    (34, "an absolutely elite"),
+    (33, "a world beating"),
+    (32, "a truly amazing"),
+    (31, "a really quite very smart"),
+    (30, "an unoverestimatable"),
+    (29, "a soon-to-be-targeted"),
+    (28, "an upper crust"),
+    (27, "a smashing"),
+    (26, "an impressive"),
+    (25, "a savvy"),
+    (24, "a weighty-brained"),
+    (23, "an algorithmically inclined"),
+    (22, "a top"),
+    (21, "a soon-to-be-red"),
+    (20, "a problem gobbling"),
+    (19, "a solid"),
+    (18, "a good smart"),
+    (17, "a tough"),
+    (16, "a two-ply extra strong"),
+    (15, "a strong"),
+    (14, "a soon-to-be-yellow"),
+    (13, "a smart"),
+    (12, "a well liked"),
+    (11, "a soon-to-be-blue"),
+    ( 5, "an up-and-coming"),
+    ( 0, "a scruffy fighter of a")
+  ]
   rating_ind = rating / 100
-  if rating_ind < 5:
-    rating_ind = 0
-  elif rating_ind < 11:
-    rating_ind = 5
-  elif rating_ind > 35:
-    rating_ind = 35
-  return compliments[rating_ind]
+  for c in compliments:
+    if rating_ind >= c[0]:
+      return c[1]
+  return "an indescribable"
 
+def rating_color(rating, target=True):
+  if rating >= 3000 and target:
+    return "a targeteer"
+  elif rating >= 2200:
+    return "red"
+  elif rating >= 1500:
+    return "yellow"
+  elif rating >= 1200:
+    return "blue"
+  elif rating >= 900:
+    return "green"
+  else:
+    return "gray"
+
+def get_accomplishments(coder, limit):
+  accomp = []
+  accomp.append("Is %s coder (rating %d)" % (get_compliment(rating), rating))
+  accomp.append("Ranks %d overall" % get_ranking(coder))
+  accomp.append("Ranks %d among coders from %s" % \
+      (get_ranking(coder, country=country), country))
+  accomp.append("Ranks %d among %s coders" % \
+      (get_ranking(coder, lang=lang), lang))
+  accomp.append("Ranks %d among %s coders from %s" % \
+      (get_ranking(coder, lang=lang, country=country), lang, country))
+  limit -= 5
+  # TODO skipped participation accomp
+  accomp.extend(get_round_accomplishments(coder, limit))
+  return accomp
+
+def get_ranking(coder, country=None, lang=None):
+  return 0 # TODO
+
+def get_round_accomplishments(coder, limit):
+  return [] # TODO
+
+# TODO PvPetr
+# TODO AsOf (return name of last SRM in db)
