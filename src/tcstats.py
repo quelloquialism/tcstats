426b152686ac3a52001c9e67dc9c2fc9191a02e0
Nathan Watson <quelloquialism@gmail.com>
2013-07-19 18:09:06 -0700

    proof-of-concept flask page for showing a coder's rating change in a single match (broken for now)

--- src/tcstats.py
+++ src/tcstats.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
-from flask import Flask, g, render_template, url_for
+from flask import Flask, g, render_template, request, url_for
 # TODO only import what is used in the end
-from flask import request, session, redirect, abort, flash
+from flask import session, redirect, abort, flash
+import sqlite3
 
 app = Flask(__name__)
 app.config.from_object("config")
 app.config.from_envvar("TCSTATS_SETTINGS", silent=True)
 
 def get_db_conn():
-  return None
+  return sqlite3.connect(app.config["SQL_DB"])
 
 @app.before_request
 def before_request():
@@ -24,9 +25,18 @@ def teardown_request(exception):
 def show_selector():
   return render_template("landing.html")
 
-@app.route("/tcstats")
+@app.route("/tcstats", methods=["GET"])
 def query_handle():
-  return render_template("tcstats.html")
+  # TODO why can't i seem to access form data? this line crashes the app
+  #handle = request.form["handle"]
+  #print handle
+  handle = "Aleksey"
+  cursor = g.conn.execute("SELECT coder_id, old_rating, new_rating " + \
+      "FROM results_50 WHERE handle=\"%s\"" % handle)
+  cid, old_rating, new_rating = cursor.fetchone()
+  dummy = ["just", "some", "words"]
+  return render_template("tcstats.html", cid=cid, handle=handle,
+      old_rating=old_rating, new_rating=new_rating, dummy=dummy)
 
 if __name__ == "__main__":
   app.run()
