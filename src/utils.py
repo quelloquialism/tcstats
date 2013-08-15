74cef8d7ae213ff602b18ee69c163c7d3ed1f406
Nathan Watson <quelloquialism@gmail.com>
2013-08-15 08:06:30 -0700

    wtf why was i ever using print

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,4 @@
 def make_row(row, format):
-  print "ROW:::", row
-  print "FORMAT:::", format
   return "<tr><td>" + \
       "</td><td>".join(format[i] % row[i] for i in range(len(row))) + \
       "</td></tr>"
@@ -14,7 +12,6 @@ def make_table(data, titles=None, format=None):
   suffix = "</table>"
   if format is None:
     format = ["%s"] * table_width
-  print "DATA:::", data
   table_body = "".join(make_row(r, format) for r in data)
   if titles is not None:
     table_body = make_row(titles, ["%s"] * table_width) + table_body
