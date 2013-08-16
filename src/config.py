6166417d53b2d740e0954a30406dc165dad3ae15
Nathan Watson <quelloquialism@gmail.com>
2013-08-15 18:33:08 -0700

    categorize config, add starting rating/vol

--- src/config.py
+++ src/config.py
@@ -9,13 +9,17 @@ FETCH_RETRIES = 5
 FETCH_TIMEOUT = 60 # seconds
 FETCH_CHUNK_SIZE = 1024 # bytes
 FETCH_SLEEP = 1 # seconds
-STATS_URL = "http://community.topcoder.com/tc?module=BasicData"
-ROUND_LIST_URL = STATS_URL + "&c=dd_round_list"
 ROUND_LIST_FILE = "/tmp/round_list.xml"
 ROUND_TABLE = "rounds"
-CODER_LIST_URL = STATS_URL + "&c=dd_coder_list"
 CODER_LIST_FILE = "/tmp/coder_list.xml"
 CODER_TABLE = "coders"
-RESULTS_URL = STATS_URL + "&c=dd_round_results&rd=%s"
 RESULTS_FILE = "/tmp/round_results_%s.xml"
 RESULTS_TABLE = "results"
+
+# The following configs are dependent on TopCoder
+STATS_URL = "http://community.topcoder.com/tc?module=BasicData"
+ROUND_LIST_URL = STATS_URL + "&c=dd_round_list"
+CODER_LIST_URL = STATS_URL + "&c=dd_coder_list"
+RESULTS_URL = STATS_URL + "&c=dd_round_results&rd=%s"
+STARTING_RATING = 1200
+STARTING_VOL = 337
